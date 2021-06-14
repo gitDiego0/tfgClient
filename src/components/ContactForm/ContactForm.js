6edcc18699cecac6b47b92e92bdf54fb6dfc881a
Diego Garcia <diegogarcianavas.4b@gmail.com>
2021-06-14 13:59:02 +0200

    axios imported

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
+
 import ComponentList from "../../utils/ComponentList";
 
 export default function ContactForm(props) {
